1a37bf4ef0a6fba1a657a1dfbd27f3e62b0d2298
Hoang Phuong <aaa@gmail.com>
2019-10-01 17:32:51 +0700

    CeilFloor

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -27,6 +27,33 @@ namespace ConsoleApplication1
             if (n % 2 == 0)
                 return true;
             return false;
-        }
+     
+       }
+       // input: float x
+       // output: int a that is smallest but not smaller than x
+       // idea: a = (int) x then a = a+1
+       // if x is negative then a is unchanged 
+       // if x==a then is unchanged 
+       public static int Ceil (float x )
+       {
+           int a = (int)x;
+           if (x > 0)
+               if (x > a)
+               a = a + 1;
+           return a;
+       }
+       // input : float x
+       // output: int a that is largest but not greater than x
+       // idea: a = (int) x
+       // if x is negative then a = a - 1
+       // if 
+
+       public static int Floor (float x)
+       {
+           int a = (int)x;
+           if (x < 0 && x != a)
+               a = a - 1;
+           return a;
+       }
     }
 }
--- ConsoleApplication1/UnitTestProject2/Properties/AssemblyInfo.cs
+++ ConsoleApplication1/UnitTestProject2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("UnitTestProject2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("UnitTestProject2")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7c12886f-db95-4208-8d2b-4a68515eac47")]
+[assembly: Guid("9913f1de-32ee-4c81-8823-1a1123be824e")]
 
 // Version information for an assembly consists of the following four values:
 //
